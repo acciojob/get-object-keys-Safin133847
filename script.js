46a4ff39767b40f899eaa7fa37f831c786658e3c
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-11-19 19:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); 
+  }
+
+  getPerimeter() {
+    return 4 * this.width; 
+  }
+}
+
+window.Rectangle = Rectangle;
+window.Square = Square;
